a95f7866e5b2716d04d543df48bd9fd6f0343405
Andi Gu <andi.gu.ca@gmail.com>
2016-11-13 00:52:22 -0500

    Added .gitignore and DatabaseAccessor (non functioning)

--- src/db/DatabaseAccessor.java
+++ src/db/DatabaseAccessor.java
@@ -1,7 +1,27 @@
 package db;
 
+import org.apache.derby.jdbc.ClientDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * @author Andi Gu
  */
 public class DatabaseAccessor {
+    private static ClientDataSource source = new ClientDataSource();
+    private static final DatabaseAccessor instance = new DatabaseAccessor();
+
+    private DatabaseAccessor() {
+        source.setDatabaseName("db");
+    }
+
+    private Connection getConnection() throws SQLException {
+        return source.getConnection();
+    }
+
+    public static DatabaseAccessor getInstance() {
+        return instance;
+    }
+
 }
