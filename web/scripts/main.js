9c08981ebbb55553e230d8dc2000d24c5b17d745
Andi Gu <andi.gu.ca@gmail.com>
2016-11-29 19:54:59 -0500

    Added bootstrap (not in use yet)

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -22,26 +22,31 @@ class ActivityViewModel {
                 }
             })
         };
-
-        Sammy(function () {
-            this.get('#login', function () {
-            });
-
-            this.get('#app', function () {
-            });
-        }).run('#login');
     }
 }
 
 
 function focus(elementId) {
+    if (elementId.charAt(0) !== '#') {
+        elementId = '#' + elementId;
+    }
+    if (elementId.substr(elementId.length - 5) !== '-view') {
+        elementId += '-view'
+    }
     const body = $('body');
     body.children('div').hide();
     body.children(elementId).show();
 }
+
 $(document).ready(function () {
     ko.applyBindings(new ActivityViewModel());
     $(window).on('hashchange', function () {
         focus(location.hash + "-view");
     });
+    if (location.hash !== '#login') {
+        location.hash = 'login';
+    }
+    else {
+        focus('#login-view');
+    }
 });
