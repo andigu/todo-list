a9fb6412080e123b2ec6126fadce68b5b417b403
susheelk <susheel.kona@gmail.com>
2016-12-10 00:03:49 -0500

    Got navigation bar working (desktop only for now)

---
+++ web/scripts/ui.js
@@ -0,0 +1,14 @@
+/**
+ * @author Susheel Kona
+ */
+"use strict";
+
+//Navbar functions
+function toggleNavbar(){
+    let navBar = document.getElementById("nav-bar");
+    if(navBar.style.display == "block"){
+        navBar.style.display = "none"
+    } else {
+        navBar.style.display = "block"
+    }
+}
